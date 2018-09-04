f8251e6a4c99450442cc9b74ec1f10ed9d4d098c
nafidurmus <nafidurmus07@gmail.com>
2018-09-04 17:46:46 +0300

    first function for Adoption.sol

---
+++ contracts/Adoption.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.19;
+
+/**
+ * The Adoption contract does this and that...
+ */
+contract Adoption {
+	address[16] public adopters;
+	//function Adoption () {
+		
+	//S}	
+	function adopt (uint petId) public returns (uint) {
+
+		require (petId >= 0 && petId <= 15);
+
+		adopters[petId] = msg.sender;
+
+		return petId;
+		
+		
+	}
+	
+}
+
