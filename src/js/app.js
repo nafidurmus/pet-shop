60d83ef207b6aa647e4da61cec5a463da4dc6562
nafidurmus <nafidurmus07@gmail.com>
2018-09-04 18:50:33 +0300

    adding web3

--- src/js/app.js
+++ src/js/app.js
@@ -24,17 +24,26 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      App.web3Provider = new Web3.prviders.HttpProvider("http://127.0.0.1:7545");
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data) {
+      var AdoptionArtifact = data;
+
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -44,21 +53,43 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+
+    var apotionInstance;
+    App.contracts.Adoption.deployed().then(function(instance) {
+      apotionInstance = instance;
+      return apotionInstance.getAdopters.call();
+    }).then(function(adopters) {
+
+      for(i =0; i< adopters.length; i++) {
+        console.log(adopters[i]);
+        if(adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
   },
 
   handleAdopt: function(event) {
     event.preventDefault();
-
+    var apotionInstance;
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
-  }
+    web3.eth.getAccounts(function(error, accounts){
+      var account = accounts[0];
 
+      App.contracts.Adoption.deployed().then(function(instance){
+        apotionInstance = instance;
+
+        return apotionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      } ).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  }
 };
 
 $(function() {
